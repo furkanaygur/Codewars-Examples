b69decd5265e88d56fe8facea3e909893fdff9b1
Furkan Aygur <32839776+furkanaygur@users.noreply.github.com>
2020-11-23 22:36:32 +0300

    Update squareNsum.py

--- squareNsum.py
+++ squareNsum.py
@@ -1,3 +1,10 @@
+'''
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+'''
+
+
 def square_sum(a):
     total = 0
     sqrt = 0
